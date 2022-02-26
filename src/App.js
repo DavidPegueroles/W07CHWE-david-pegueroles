1c348e44ec3c96671cec7520a2d5de03cc609418
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-26 22:02:32 +0100

    Added a render of the RegisterPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
 function App() {
   return (
     <>
       <h1>Site in progress, will be available soon...</h1>
+
+      <RegisterPage />
     </>
   );
 }
