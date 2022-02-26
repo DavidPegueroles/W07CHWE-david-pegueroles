7b8c785582cab8f704b28f8afd6ce1b705000542
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-26 20:45:09 +0100

    Added tests of userRegister and default useerReducer cases

---
+++ src/redux/reducers/userReducers.test.js
@@ -0,0 +1,56 @@
+import actionsTypes from "../actions/actionsTypes";
+import userReducers from "./userReducers";
+
+describe("Given a userReducers function", () => {
+  describe("When it receives currentUser and userRegister action with a valid user", () => {
+    test("Then it should return the user", () => {
+      const currentUser = {};
+
+      const user = {
+        name: "admin",
+        username: "admin",
+        password: "Admin123",
+      };
+
+      const action = {
+        type: actionsTypes.userRegister,
+        user,
+      };
+
+      const reducerResult = userReducers(currentUser, action);
+
+      expect(reducerResult).toEqual(user);
+    });
+  });
+
+  describe("When it receives currentUser and a non existing action with a valid user", () => {
+    test("Then it should return currentUser", () => {
+      const currentUser = {};
+
+      const user = {
+        name: "admin",
+        username: "admin",
+        password: "Admin123",
+      };
+
+      const action = {
+        type: actionsTypes.unexisting,
+        user,
+      };
+
+      const reducerResult = userReducers(currentUser, action);
+
+      expect(reducerResult).toEqual(currentUser);
+    });
+  });
+
+  describe("When it doesn't receive any user nor action", () => {
+    test("Then it should return an empty object", () => {
+      const expectedResult = {};
+
+      const reducerResult = userReducers();
+
+      expect(reducerResult).toEqual(expectedResult);
+    });
+  });
+});
