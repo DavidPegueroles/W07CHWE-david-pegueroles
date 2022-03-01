518ca7daf98ad3711096cefe4e023dcce4c96482
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-01 02:02:00 +0100

    Added userLoginThunk function

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -1,4 +1,8 @@
-import { userRegisterAction } from "../actions/actionsCreators";
+import jwtDecode from "jwt-decode";
+import {
+  userLoginAction,
+  userRegisterAction,
+} from "../actions/actionsCreators";
 
 export const userRegisterThunk = (user) => async (dispatch) => {
   const response = await fetch(`${process.env.REACT_APP_RRSS}users/register`, {
@@ -10,4 +14,23 @@ export const userRegisterThunk = (user) => async (dispatch) => {
   });
   const newUser = await response.json();
   dispatch(userRegisterAction(newUser));
+};
+
+export const userLoginThunk = (user) => async (dispatch) => {
+  const response = await fetch(`${process.env.REACT_APP_RRSS}users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    const { token } = await response.json();
+    const { id, username } = await jwtDecode(token);
+
+    localStorage.setItem("userToken", token);
+
+    dispatch(userLoginAction({ id, username, token }));
+  }
 };
